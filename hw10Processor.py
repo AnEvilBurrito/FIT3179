82a0e8b083466205a491b4e59be7f9c93c17c0d6
Dawson <ylan0001@student.monash.edu>
2020-10-21 18:33:23 +1100

    update

--- hw10Processor.py
+++ hw10Processor.py
@@ -3,19 +3,24 @@ import pandas as pd
 fb = pd.read_csv("FB.csv")
 aapl = pd.read_csv("AAPL.csv")
 msft = pd.read_csv("MSFT.csv")
+nflx = pd.read_csv("NFLX.csv")
+goog = pd.read_csv("GOOG.csv")
 
 dateData = fb["Date"]
 fbPriceData = fb["Close"] 
 aaplPriceData = aapl["Close"]
 msftPriceData = msft["Close"]
+nflxPriceData = nflx["Close"]
+googPriceData = goog["Close"]
 
-symbols = ["FB", "AAPL", "MSFT"]
+symbols = ["FB", "AAPL", "MSFT", "NFLX", "GOOG"]
 
 data = []
 i = 0 
 while i < len(fbPriceData):
     fbPrice, aaplPrice, msftPrice, date = fbPriceData[i], aaplPriceData[i], msftPriceData[i], dateData[i]
-    pSet = [fbPrice, aaplPrice, msftPrice] 
+    nflxPrice, googPrice = nflxPriceData[i], googPriceData[i]
+    pSet = [fbPrice, aaplPrice, msftPrice, nflxPrice, googPrice] 
     for j in range(len(symbols)):
         symbol = symbols[j]
         p = pSet[j]
